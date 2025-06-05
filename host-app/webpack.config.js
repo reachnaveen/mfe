2dd1c6306a7ff34d7c20102cd67ff8e13176643d
Naveen <naveen.kamrudeen@arrivia.com>
2025-06-05 09:11:08 -0700

    Update webpack.config.js

--- host-app/webpack.config.js
+++ host-app/webpack.config.js
@@ -33,12 +33,9 @@ module.exports = {
     new ModuleFederationPlugin({
       name: 'host',
       remotes: {
-        // productFilter: 'productFilter@http://localhost:3004/remoteEntry.js',
-        // productSearch: 'productSearch@http://localhost:3001/remoteEntry.js',
-        // details: 'details@http://localhost:3002/remoteEntry.js',
-        productFilter: 'productFilter@http://filter.arrivia.com:8084/remoteEntry.js',
-        productSearch: 'productSearch@http://search.arrivia.com:8084/remoteEntry.js',
-        details: 'details@http://details.arrivia.com:8084/remoteEntry.js',
+        productFilter: 'productFilter@http://localhost:3004/remoteEntry.js',
+        productSearch: 'productSearch@http://localhost:3001/remoteEntry.js',
+        details: 'details@http://localhost:3002/remoteEntry.js',
       },
       shared: {
         react: { singleton: true, requiredVersion: '^18.2.0', eager: true },
